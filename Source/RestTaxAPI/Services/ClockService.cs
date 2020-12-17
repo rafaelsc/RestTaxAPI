05ab4782a6ef9f05df11f4e126934c5911617c00
Rafael Cordeiro <rafaelsc@rafaelsc.net>
2020-12-16 23:17:15 -0800

    Remove template Code

--- Source/RestTaxAPI/Services/ClockService.cs
+++ Source/RestTaxAPI/Services/ClockService.cs
@@ -5,6 +5,14 @@ namespace RestTaxAPI.Services
     /// <summary>
     /// Retrieves the current date and/or time. Helps with unit testing by letting you mock the system clock.
     /// </summary>
+    public interface IClockService
+    {
+        DateTimeOffset UtcNow { get; }
+    }
+
+    /// <summary>
+    /// Retrieves the current date and/or time. Helps with unit testing by letting you mock the system clock.
+    /// </summary>
     public class ClockService : IClockService
     {
         public DateTimeOffset UtcNow => DateTimeOffset.UtcNow;
