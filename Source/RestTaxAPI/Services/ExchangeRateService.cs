01f84d1c5ac909113bbf69e0486ff9c0c26558ea
Rafael Cordeiro <rafaelsc@rafaelsc.net>
2020-12-17 14:54:06 -0800

    Implement ExchangeRateService Improve Cached ExchangeRateService

--- Source/RestTaxAPI/Services/ExchangeRateService.cs
+++ Source/RestTaxAPI/Services/ExchangeRateService.cs
@@ -4,6 +4,10 @@ namespace RestTaxAPI.Services
     using System.Collections;
     using System.Collections.Concurrent;
     using System.Collections.Generic;
+    using System.Linq;
+    using FixerSharp;
+    using Models;
+    using Repositories;
 
     /// <summary>
     /// TODO
@@ -21,26 +25,47 @@ namespace RestTaxAPI.Services
 
     internal class ExchangeRateService : IExchangeRateService
     {
+        public ExchangeRateService()
+        {
+            Fixer.SetApiKey("70917b78c21d431cd1f5f16e2763a551");
+        }
+
         public decimal GetExchangeRate(string sourceCurrency, string destinationCurrency)
         {
             if (sourceCurrency == destinationCurrency)
                 return 1;
 
-            throw new NotImplementedException(); //TODO Call the fixer.io API to get the current Rates
+            var rate = Fixer.Rate(sourceCurrency, Symbols.GBP);
+            var exchangeRate = rate.Convert(1);
+            return (decimal)exchangeRate;
         }
     }
 
     internal class CachedExchangeRateService : IExchangeRateService //TODO Test this Cached Service.
     {
-        private const int CacheTimeoutInSeconds = 60; //TODO: Add this to a config file.
+        private static TimeSpan CacheTimeout => TimeSpan.FromHours(1); //TODO: Add this to a config file.
 
-        private ConcurrentDictionary<(string, string), (DateTimeOffset, decimal)> Cache = new ConcurrentDictionary<(string, string), (DateTimeOffset, decimal)>();
-        
+        private static readonly ConcurrentDictionary<(string, string), (DateTimeOffset, decimal)> Cache = new();
+
         private ExchangeRateService ExchangeRateService { get; }
 
-        public CachedExchangeRateService(ExchangeRateService exchangeRateService)
+        public CachedExchangeRateService(ExchangeRateService exchangeRateService, ICurrencyRepository currencyRepository)
         {
             this.ExchangeRateService = exchangeRateService;
+            this.FillCache(currencyRepository.GetAllAllowed().ToArray());
+        }
+
+        private void FillCache(Currency[] currencies)
+        {
+            var allCommutations = from c1 in currencies
+                                  from c2 in currencies
+                                  where c1.Code != c2.Code
+                                  select (Source: c1.Code, Destination: c2.Code);
+
+            foreach (var curr in allCommutations)
+            {
+                this.GetExchangeRate(curr.Source, curr.Destination);
+            }
         }
 
         public decimal GetExchangeRate(string sourceCurrency, string destinationCurrency)
@@ -48,14 +73,14 @@ namespace RestTaxAPI.Services
             if (sourceCurrency == destinationCurrency)
                 return 1;
 
-            var cachedData = this.Cache.GetOrAdd((sourceCurrency, destinationCurrency), (i) => (DateTimeOffset.UtcNow, this.ExchangeRateService.GetExchangeRate(i.Item1, i.Item2)));
-            if (DateTimeOffset.UtcNow - cachedData.Item1 < TimeSpan.FromSeconds(CacheTimeoutInSeconds))
+            var cachedData = Cache.GetOrAdd((sourceCurrency, destinationCurrency), (i) => (DateTimeOffset.UtcNow, this.ExchangeRateService.GetExchangeRate(i.Item1, i.Item2)));
+            if (DateTimeOffset.UtcNow - cachedData.Item1 < CacheTimeout)
                 return cachedData.Item2; //Return the Cached rate;
 
             //TODO, BUG, If the ExchangeRateService.GetExchangeRate return slower than CacheTimeoutInSeconds a cache miss will happen.
 
             var currentRate = this.ExchangeRateService.GetExchangeRate(sourceCurrency, destinationCurrency);
-            this.Cache[(sourceCurrency, destinationCurrency)] = (DateTimeOffset.UtcNow, currentRate); //Update rate in Cache
+            Cache[(sourceCurrency, destinationCurrency)] = (DateTimeOffset.UtcNow, currentRate); //Update rate in the Cache
             return currentRate;
         }
     }
